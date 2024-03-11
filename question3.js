27cc04d7fcfdabd3238a2b5ad230b2a774691c15
Aliya <160420010+Aliyaasghar@users.noreply.github.com>
2024-03-11 11:17:03 +0500

    Add files via upload

---
+++ question3.js
@@ -0,0 +1,9 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var personName = "aliya";
+// // lower case
+// console.log(personName.toLowerCase());
+// // uppercase
+// console.log(personName.toLocaleUpperCase());
+// title case
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1, 5));
+console.log();
