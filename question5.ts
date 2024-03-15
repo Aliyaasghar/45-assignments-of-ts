4f4c94527f8088e1513e85de3d60101150dad438
Aliya <160420010+Aliyaasghar@users.noreply.github.com>
2024-03-15 14:13:42 +0500

    Add files via upload

---
+++ question5.ts
@@ -0,0 +1,10 @@
+
+let famouspersons :string = "Quaid-e-Azam";
+let famousquote:string = '"I do not believe in taking the right decision, I take a decision and make it right."';
+
+// compose message
+
+let message1:string = `${famouspersons} once said , ${famousquote}`;
+console.log(message);
+
+
