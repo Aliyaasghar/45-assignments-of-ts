11f25e5cb63232015d505860106f4684f5f0e03c
Aliya <160420010+Aliyaasghar@users.noreply.github.com>
2024-03-17 08:11:19 +0500

    Add files via upload

---
+++ question3.ts
@@ -0,0 +1,19 @@
+ let personName:string = "aliya";
+// // lower case
+ console.log(personName.toLowerCase());
+// // uppercase
+ console.log(personName.toLocaleUpperCase());
+// title case
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1,5));
+
+
+
+
+
+
+
+
+
+
+
+
