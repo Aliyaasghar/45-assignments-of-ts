13ecf521dff658402ff2833674dbd19977f7b499
Aliya <160420010+Aliyaasghar@users.noreply.github.com>
2024-03-19 01:15:07 +0500

    Add files via upload

---
+++ question12.ts
@@ -0,0 +1,12 @@
+// make array
+
+let names1:string[]=["aliya", "aleena",  "muniza"];
+//  send message
+let message :string = "how are you";
+
+console.log(names1[0] + " " + message);
+console.log(names1[1] + " " + message);
+console.log(names1[2] + " " + message);
+
+
+
