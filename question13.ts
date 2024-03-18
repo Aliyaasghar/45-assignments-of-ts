fa57a9640f5621755278ffdbbf04ba14f9ee6c9d
Aliya <160420010+Aliyaasghar@users.noreply.github.com>
2024-03-19 01:15:44 +0500

    Add files via upload

---
+++ question13.ts
@@ -0,0 +1,11 @@
+let Fvrt_transportation :string[] = ["honda motorcycle", "civic", "fortuner", "audi"];
+
+// let statement:string= "i would like to own a"
+
+// console.log(statement + " " + Fvrt_transportation[0]);
+// console.log(statement + " " + Fvrt_transportation[1]);
+// console.log(statement + " " + Fvrt_transportation[2]);
+// console.log(statement + " " + Fvrt_transportation[3]);
+
+// easy way
+Fvrt_transportation.map((item)=> console.log("I would like to own A",item));
