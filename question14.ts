829787c1a1476a8d769be8ddb67a2aae6950440f
Aliya <160420010+Aliyaasghar@users.noreply.github.com>
2024-03-19 01:16:14 +0500

    Add files via upload

---
+++ question14.ts
@@ -0,0 +1,14 @@
+
+let guestlist:string[] =[ "grand parents", "uncle aunt", "mamu"]
+
+// guestlist.map((item)=>console.log("I would like to invite you in dinner",item));
+
+// map methode
+guestlist.map((item)=>console.log(`Hey ${item} you are invited to dinner`));
+
+
+
+
+
+
+
