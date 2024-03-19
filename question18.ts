eab97c86149e5bda28a3f97120d084b161a1a8da
Aliya <160420010+Aliyaasghar@users.noreply.github.com>
2024-03-19 06:26:53 +0500

    Add files via upload

---
+++ question18.ts
@@ -0,0 +1,14 @@
+let visitplace:string[]=["indonesia", "bali", "turkey", "uk", "canada"];
+console.log(visitplace);
+console.log(visitplace.sort());
+console.log(visitplace);
+console.log(visitplace.sort().reverse());
+console.log(visitplace);
+console.log(visitplace.reverse());
+console.log(visitplace.reverse());
+let sortedArray = ["indonesia", "bali", "turkey", "uk", "canada"];
+sortedArray.sort();
+console.log(sortedArray);
+sortedArray.reverse();
+console.log(sortedArray);
+
